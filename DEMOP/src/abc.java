21350556053334fb3a2001b33ef71c5ec4e540a8
Sumahyd <suma.bezwada9@gmail.com>
2018-01-08 13:22:59 +0530

    new chnage

---
+++ DEMOP/src/abc.java
@@ -0,0 +1,18 @@
+
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class abc {
+	
+	public static void main(String args[]){  
+		  HashMap<Integer,String> hm=new HashMap<Integer,String>();  
+		  hm.put(100,"Amit");  
+		  hm.put(101,"Vijay");  
+		  hm.put(102,"Rahul");  
+		  for(Entry<Integer, String> m:hm.entrySet()){  
+		   System.out.println(m.getKey()+" "+m.getValue());  
+		  }  
+		 }  
+
+}
